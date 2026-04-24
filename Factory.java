f9572a5db164075553597f5569530d852ffd5f38
amrmahmoud01 <amrsallam2001@gmail.com>
2026-04-24 18:40:09 +0300

    removed factory class. Main acts as the factory (pls hire me)

--- Factory.java
+++
@@ -1,8 +0,0 @@
-public class Factory {
-    public Car createCar(String engineType){
-        return new Car(engineType);
-    }
-    public void replaceEngine(Car car, String engineType){
-        car = new Car(engineType);
-    }
-}
